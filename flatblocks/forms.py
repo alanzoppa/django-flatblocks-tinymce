f0a74ea82aa3e54440d6f5e72dbeb6099f085af5
Alan Zoppa <azoppa@Manifest-Digitals-MacBook-Pro-5.local>
2010-07-30 11:39:38 -0500

    Should only use Tiny_MCE if available

--- flatblocks/forms.py
+++ flatblocks/forms.py
@@ -1,11 +1,16 @@
 from django.forms import ModelForm
 from django import forms
-from tinymce.widgets import TinyMCE
-
 from flatblocks.models import FlatBlock
 
+try:
+    from tinymce.widgets import TinyMCE
+    tiny_mce = True
+except ImportError:
+    tiny_mce = False
+
 class FlatBlockForm(ModelForm):
-    content = forms.CharField(widget=TinyMCE(), required=False)
+    if tiny_mce:
+        content = forms.CharField(widget=TinyMCE(), required=False)
     class Meta:
         model = FlatBlock
         #exclude = ('slug', )
