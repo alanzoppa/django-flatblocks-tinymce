bccfbad9dcf7b40a3a2107d0c7fd27d15faa36b8
Alan Zoppa <azoppa@Manifest-Digitals-MacBook-Pro-5.local>
2010-07-30 11:36:10 -0500

    Updated to use TinyMCE modelform

--- flatblocks/admin.py
+++ flatblocks/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from flatblocks.models import FlatBlock
+from flatblocks.forms import FlatBlockForm
  
 class FlatBlockAdmin(admin.ModelAdmin):
     ordering = ['slug',]
     list_display = ('slug', 'header', 'content')
     search_fields = ('slug', 'header', 'content')
+    form = FlatBlockForm
+
 
 admin.site.register(FlatBlock, FlatBlockAdmin)
--- flatblocks/forms.py
+++ flatblocks/forms.py
@@ -1,8 +1,11 @@
 from django.forms import ModelForm
+from django import forms
+from tinymce.widgets import TinyMCE
 
 from flatblocks.models import FlatBlock
 
 class FlatBlockForm(ModelForm):
+    content = forms.CharField(widget=TinyMCE(), required=False)
     class Meta:
         model = FlatBlock
-        exclude = ('slug', )
+        #exclude = ('slug', )
